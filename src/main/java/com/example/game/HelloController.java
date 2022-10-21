e29e136e7f8167e7c2fff0454bd8bad4efffccaa
Paula <paula@pop-os.localdomain>
2022-10-21 14:17:24 +0200

    refactored racket logic on movement

--- src/main/java/com/example/game/HelloController.java
+++ src/main/java/com/example/game/HelloController.java
@@ -1,34 +1,22 @@
 package com.example.game;
 
 import javafx.animation.Animation;
-import javafx.animation.AnimationTimer;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.Shape;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
 
 public class HelloController {
+
+    PingPongBall pingPongBall;
+    Racket firstRacket, secondRacket;
+    private static final int SPEED = 5;
     @FXML
     public Rectangle ball ;
-    PingPongBall pingPongBall;
-
-    private boolean upIsPressed = false;
-
-    private static final float RACKET_TOP_BOUNDARY  = 300;
-    private static final float RACKET_BOTTOM_BOUNDARY  = 10; // TODO : move to another class ...
-    private boolean downIsPressed = false;
-    private boolean sIsPressed = false;
-    private boolean wIsPressed = false;
-
-    private static final int SPEED = 5;
-
-
     @FXML
     public Rectangle secondPlayerRacket, firstPlayerRacket, topBorder, bottomBorder;
     @FXML
@@ -37,53 +25,21 @@ public class HelloController {
 
     @FXML
     private void handleKeyPressed(KeyEvent ke){
-        if (ke.getCode() == KeyCode.DOWN || this.downIsPressed) {
-            // Move racket down
-            this.downIsPressed = true;
-            double racket = firstPlayerRacket.getLayoutY();
-            if (racket < RACKET_TOP_BOUNDARY ){
-                firstPlayerRacket.setLayoutY(firstPlayerRacket.getLayoutY() + 10);
-            }
-            // Move racket down
-        }
-
-        if (ke.getCode() == KeyCode.UP || this.upIsPressed) {
-            // Move racket up
-            this.upIsPressed = true;
-            double racket = firstPlayerRacket.getLayoutY();
-            if (racket > RACKET_BOTTOM_BOUNDARY ){
-                firstPlayerRacket.setLayoutY(firstPlayerRacket.getLayoutY() - 10);
-            }
-            // Move racket up
-        }
-
-        if (ke.getCode() == KeyCode.S || this.sIsPressed) {
-            // Move racket down
-            this.sIsPressed = true;
-            double racket = secondPlayerRacket.getLayoutY();
-            if (racket < RACKET_TOP_BOUNDARY){
-                secondPlayerRacket.setLayoutY(secondPlayerRacket.getLayoutY() + 10);
-            }
-            // Move racket down
-        }
-
-        if (ke.getCode() == KeyCode.W || this.wIsPressed) {
-            // Move racket down
-            this.wIsPressed = true;
-            double racket = secondPlayerRacket.getLayoutY();
-            if (racket > 10 ) {
-                secondPlayerRacket.setLayoutY(secondPlayerRacket.getLayoutY() - 10);
-            }
-            // Move racket down
-        }
+        this.firstRacket.registerUpMovement(ke)
+                .registerDownMovement(ke);
+        this.secondRacket.registerUpMovement(ke)
+                .registerDownMovement(ke);
     }
 
 
     @FXML
     public void initialize(){
+        this.firstRacket = new FirstPlayerRacket(firstPlayerRacket);
+        this.secondRacket = new SecondPlayerRacket(secondPlayerRacket);
         this.pingPongBall = new PingPongBall(this.ball);
         Timeline timeline = new Timeline(new KeyFrame(Duration.millis(SPEED), event -> {
             if (ball.getBoundsInParent().intersects(firstPlayerRacket.getBoundsInParent()) ) {
+                // TODO : refactor in the future ...
                 this.pingPongBall.setRacket(firstPlayerRacket);
                 this.pingPongBall.determineMovementOfBall();
             }
@@ -110,21 +66,8 @@ public class HelloController {
 
     @FXML
     private void handleKeyReleased(KeyEvent ke) {
-        if (ke.getCode() == KeyCode.DOWN) {
-            this.downIsPressed = false;
-        }
-
-        if (ke.getCode() == KeyCode.UP) {
-            this.upIsPressed = false;
-        }
-
-        if (ke.getCode() == KeyCode.S) {
-            this.sIsPressed = false;
-        }
-
-        if (ke.getCode() == KeyCode.W) {
-            this.wIsPressed = false;
-        }
+        this.firstRacket.resetClicks(ke);
+        this.secondRacket.resetClicks(ke);
     }
 
 }
--- src/main/java/com/example/game/Racket.java
+++ src/main/java/com/example/game/Racket.java
@@ -1,27 +1,31 @@
 package com.example.game;
 
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.shape.Rectangle;
 
-public class Racket {
+abstract class Racket {
     private static final float RACKET_TOP_BOUNDARY  = 300;
     private static final float RACKET_BOTTOM_BOUNDARY  = 10;
-    private boolean downIsPressed;
-    public Rectangle racket;
+    private static final int RACKET_MOVING_RATE = 10;
 
-    public void moveRacketDown(KeyEvent ke){
-        if (ke.getCode() == KeyCode.DOWN || this.downIsPressed) {
 
-            this.downIsPressed = true;
-            double racket = this.racket.getLayoutY();
-            if (racket < RACKET_TOP_BOUNDARY) {
-                this.racket.setLayoutY(this.racket.getLayoutY() + 10);
-            }
+    protected void downMovement(Rectangle racket) {
+        double racketLayoutY = racket.getLayoutY();
+        if (racketLayoutY < RACKET_TOP_BOUNDARY) {
+            racket.setLayoutY(racket.getLayoutY() + RACKET_MOVING_RATE);
         }
     }
 
-    public void moveRacketUp() {
-
+    protected void upMovement(Rectangle racket) {
+        double racketLayoutY = racket.getLayoutY();
+        if (racketLayoutY > RACKET_BOTTOM_BOUNDARY){
+            racket.setLayoutY(racket.getLayoutY() - RACKET_MOVING_RATE);
+        }
     }
+
+    public abstract Racket registerDownMovement(KeyEvent ke);
+    public abstract Racket registerUpMovement(KeyEvent ke);
+
+    public abstract void resetClicks(KeyEvent ke);
+
 }
