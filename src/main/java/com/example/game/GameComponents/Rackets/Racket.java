ba08243f619dd0777e4ab3d8a0d87d8870b9a6ce
Paula <paula@pop-os.localdomain>
2022-11-03 13:43:57 +0100

    added inner classes

--- src/main/java/com/example/game/GameComponents/Rackets/Racket.java
+++ src/main/java/com/example/game/GameComponents/Rackets/Racket.java
@@ -1,5 +1,6 @@
 package com.example.game.GameComponents.Rackets;
 
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.shape.Rectangle;
 
@@ -15,7 +16,7 @@ public abstract class Racket {
             racket.setLayoutY(racket.getLayoutY() + RACKET_MOVING_RATE);
         }
     }
-
+    
     protected void upMovement(Rectangle racket) {
         double racketLayoutY = racket.getLayoutY();
         if (racketLayoutY > RACKET_BOTTOM_BOUNDARY){
@@ -27,5 +28,80 @@ public abstract class Racket {
     public abstract Racket registerUpMovement(KeyEvent ke);
 
     public abstract void resetClicks(KeyEvent ke);
+
+
+
+
+    public class SecondPlayerRacket extends Racket{
+        private final Rectangle racket;
+        private boolean sIsPressed = false, wIsPressed = false;
+
+        public SecondPlayerRacket(Rectangle racket) {
+            this.racket = racket;
+        }
+
+        public SecondPlayerRacket registerDownMovement(KeyEvent ke){
+            if (ke.getCode() == KeyCode.S || this.sIsPressed) {
+                this.sIsPressed = true;
+                this.downMovement(this.racket);
+            }
+            return this;
+        }
+
+        public SecondPlayerRacket registerUpMovement(KeyEvent ke){
+            if (ke.getCode() == KeyCode.W || this.wIsPressed) {
+                this.wIsPressed = true;
+                this.upMovement(this.racket);
+            }
+            return this;
+        }
+
+        public void resetClicks(KeyEvent ke){
+
+            if (ke.getCode() == KeyCode.S) {
+                this.sIsPressed = false;
+            }
+
+            if (ke.getCode() == KeyCode.W) {
+                this.wIsPressed = false;
+            }
+        }
+    }
+
+
+    public class FirstPlayerRacket extends Racket{
+        private final Rectangle racket;
+        private boolean downIsPressed = false, upIsPressed = false;
+
+        public FirstPlayerRacket(Rectangle racket) {
+            this.racket = racket;
+        }
+
+        public FirstPlayerRacket registerDownMovement(KeyEvent ke){
+            if (ke.getCode() == KeyCode.DOWN || this.downIsPressed) {
+                this.downIsPressed = true;
+                this.downMovement(this.racket);
+            }
+            return this;
+        }
+
+        public FirstPlayerRacket registerUpMovement(KeyEvent ke){
+            if (ke.getCode() == KeyCode.UP || this.upIsPressed) {
+                this.upIsPressed = true;
+                this.upMovement(this.racket);
+            }
+            return this;
+        }
+
+        public void resetClicks(KeyEvent ke){
+            if (ke.getCode() == KeyCode.DOWN) {
+                this.downIsPressed = false;
+            }
+
+            if (ke.getCode() == KeyCode.UP) {
+                this.upIsPressed = false;
+            }
+        }
+    }
 
 }
